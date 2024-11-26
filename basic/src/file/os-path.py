7f137f160bf56334f571ce66ea15a4fdacb17ea9
shiloh <lixiaolei595@gmail.com>
2024-11-26 22:18:39 +0800

    feat: add file and dir examples

--- basic/src/file/os-path.py
+++ basic/src/file/os-path.py
@@ -2,13 +2,17 @@
 python 文件路径操作 - os.path
 """
 
+# shutil 模块提供了复制文件、目录的函数
 import os
+import shutil
+
+TEST_FILE_PATH = "./test.txt"
 
 #  路径拼接
 print(os.path.join("usr", "bin", "spam"))
 
 my_files = ["a.txt", "b.txt", "c.txt"]
-dir_name = "D:\\shiloh\\develop"
+dir_name = "C:\\Users\\shiloh\\code"
 for filename in my_files:
     print(os.path.join(dir_name, filename))
 
@@ -16,8 +20,63 @@ for filename in my_files:
 print(os.getcwd())
 
 # 切换当前工作路径
-os.chdir(dir_name)
-print(os.getcwd())
+# os.chdir(dir_name)
+# print(os.getcwd())
 
 # 创建新目录
-os.makedirs(f"{dir_name}\\test_dir")
+# os.makedirs(f"{dir_name}\\test_dir")
+
+# 绝对路径和相对路径
+# 绝对路径总是以根路径开始
+# 相对路径是相对于程序当前工作路径
+# . 代表当前目录，.. 代表上级目录
+
+# 判断是否为绝对路径
+print(os.path.isabs("C://"))
+print(os.path.isabs("./"))
+
+# 获取当前所在目录的绝对路径
+print(os.getcwd())
+
+# 获取上级目录的绝对路径
+print(os.path.abspath(".."))
+
+# 获取相对路径：指定A路径、获取它相对于B路径的相对路径
+TEST_DIR_PATH = "C:\\Users"
+print(os.path.relpath(TEST_DIR_PATH, "C:\\"))
+
+# 文件/路径检查 - 检查文件/路径是否存在
+print(os.path.exists("."))
+print(os.path.exists("./non_existent_file.py"))
+
+# 检查路径是否为文件
+print(os.path.isfile(TEST_DIR_PATH))
+print(os.path.isfile(TEST_FILE_PATH))
+print(os.path.isfile("./non_existent_file.py"))
+
+# 检查路径是否为目录
+print(os.path.isdir(TEST_DIR_PATH))
+print(os.path.isdir(TEST_FILE_PATH))
+
+# 获取文件大小，单位：bytes
+print(f"文件：{TEST_FILE_PATH} 大小为：{os.path.getsize(TEST_FILE_PATH)} 字节")
+
+# 列出指定目录下面的所有文件/子目录
+print(os.listdir(TEST_DIR_PATH))
+for f in os.listdir(TEST_DIR_PATH):
+    # 输出的仅是文件名/子目录名
+    print(f)
+
+# 计算指定目录下的文件大小
+total_size = 0
+for filename in os.listdir(TEST_DIR_PATH):
+    total_size += os.path.getsize(os.path.join(TEST_DIR_PATH, filename))
+
+print(f"{total_size=}")
+
+# 复制文件和目录
+shutil.copy(TEST_FILE_PATH, TEST_DIR_PATH)
+# 复制整个目录以及该目录下的所有文件
+shutil.copytree("./test", os.path.join(TEST_DIR_PATH, "test"))
+
+# https://www.pythoncheatsheet.org/cheatsheet/file-directory-path#moving-and-renaming
--- basic/src/file/pathlib.py
+++ basic/src/file/pathlib.py
@@ -2,9 +2,12 @@
 python 文件路径操作 - pathlib
 pathlib 是 python3.4 新增的，提供以面向对象的方式操作文件系统路径
 """
-import os
 
-from pathlib import Path
+from pathlib2 import Path
+
+TEST_FILE_PATH = "./test.txt"
+
+TEST_DIR_PATH = "C:\\Users\\shiloh"
 
 #  路径拼接，python 会自动处理不同平台上的文件路径分隔符
 print(Path("usr") / "bin" / "spam")
@@ -17,10 +20,58 @@ for filename in my_files:
 # 获取当前工作路径
 print(Path.cwd())
 # 修改当前工作路径
-os.chdir("D:\\shiloh\\develop")
-print(Path.cwd())
+# os.chdir("C:\\Users\\shiloh\\code")
+# print(Path.cwd())
 
 # 创建新目录
-cwd = Path.cwd()
+# cwd = Path.cwd()
 # mkdir(parents=True) 当父目录不存在时，会自动创建
-(cwd / "test_dir" / "sub_dir").mkdir(parents=True)
+# (cwd / "test_dir" / "sub_dir").mkdir(parents=True)
+
+# 绝对路径和相对路径
+# 绝对路径总是以根路径开始
+# 相对路径是相对于程序当前工作路径
+# . 代表当前目录，.. 代表上级目录
+
+# 判断是否为绝对路径
+print(Path("C:\\").is_absolute())
+print(Path("..").is_absolute())
+
+# 获取当前所在目录的绝对路径
+print(Path.cwd())
+
+# 获取上级目录的绝对路径
+print(Path("..").resolve())
+
+# 获取相对路径：指定A路径、获取它相对于B路径的相对路径
+print(Path(TEST_DIR_PATH).relative_to("C:\\"))
+
+# 文件/路径检查 - 检查文件/路径是否存在
+print(Path(".").exists())
+print(Path("./non_existent_file.py").exists())
+
+# 检查路径是否为文件
+print(Path(TEST_DIR_PATH).is_file())
+print(Path(TEST_FILE_PATH).is_file())
+print(Path("./non_existent_file.py").is_file())
+
+# 检查路径是否为目录
+print(Path(TEST_DIR_PATH).is_dir())
+print(Path(TEST_FILE_PATH).is_dir())
+
+# 获取文件大小，单位：bytes
+stat = Path(TEST_FILE_PATH).stat()
+print(stat)
+print(stat.st_size)
+
+# 列出指定目录下面的所有文件/子目录
+for f in Path(TEST_DIR_PATH).iterdir():
+    # 输出的是文件/子目录的绝对路径
+    print(f)
+
+# 计算指定目录下的文件大小
+total_size = 0
+for sub_path in Path(TEST_DIR_PATH).iterdir():
+    total_size += sub_path.stat().st_size
+
+print(f"{total_size=}")
