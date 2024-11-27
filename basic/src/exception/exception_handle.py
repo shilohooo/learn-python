421c6d3ae7bf6b9b14d19effee2bd725facf659d
shiloh <lixiaolei595@gmail.com>
2024-11-27 17:31:47 +0800

    feat: add exception handling example

--- basic/src/exception/exception_handle.py
+++ basic/src/exception/exception_handle.py
@@ -13,3 +13,29 @@ except ZeroDivisionError:
 else:
     # 业务代码执行完没有发送异常，会走到这里~ 
     print('没有异常发生。')
+finally:
+    print('无论是否发生异常，都会执行。')
+
+
+# 在同一个 except 代码块中处理多个异常
+def divide(dividend, divisor):
+    try:
+        # var = 'str' + 1
+        print(dividend / divisor)
+    except(ZeroDivisionError, TypeError) as e:
+        print(e)
+
+
+divide(dividend=10, divisor=5)
+divide(dividend=10, divisor=0)
+
+
+#  自定义异常
+class MyException(Exception):
+    pass
+
+
+try:
+    raise MyException('抛出自定义异常')
+except MyException as e:
+    print('捕获自定义异常')
